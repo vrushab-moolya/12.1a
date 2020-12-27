c301c703c586e576cb85b6ee130f741ede87ee3b
vrushab-moolya <vrushab.patil@moolya.com>
2020-12-27 12:27:53 +0530

    second commit

--- src/main/java/api/Apimethods.java
+++ src/main/java/api/Apimethods.java
@@ -1,5 +1,6 @@
 package api;
 import static io.restassured.RestAssured.*;
+
 public class Apimethods {
     public static void postmethod(String url){
         given().log().all().body("{\n \n first_name:Manasa,last_name:M \n").post(url).then().log().all();
--- src/test/java/Test/Sample2.java
+++ src/test/java/Test/Sample2.java
@@ -2,7 +2,7 @@ package Test;
 
 import api.Apimethods;
 import base.Basemethods;
-import org.junit.Test;
+
 import util.Utilmethods;
 
 
@@ -10,28 +10,42 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 public class Sample2 {
     @Test
     public void testing2(){
+        //website
         String url="https://www.guru99.com/";
-        Basemethods.getmethod(url);
-        Apimethods.postmethod(url);
-        Utilmethods.getresponsetime(url);
-    }
 
-    @Test
-    public void apiTest(){
+        //extent report
         ExtentHtmlReporter reporter=new ExtentHtmlReporter("./ExtentReports/test4.html");
         ExtentReports extent=new ExtentReports();
         extent.attachReporter(reporter);
-        ExtentTest logger = extent.createTest("apitest");
-        logger.log(Status.INFO,"API CALL");
-        logger.log(Status.PASS,"API TEST PASSED");
+
+        //base package
+        ExtentTest logger1 = extent.createTest("base package");
+        Basemethods.getmethod(url);
+        logger1.log(Status.INFO,"API CALL");
+        logger1.log(Status.PASS,"API TEST PASSED");
         extent.flush();
 
+        //api package
+        ExtentTest logger2 = extent.createTest("api package");
+        Apimethods.postmethod(url);
+        logger2.log(Status.INFO,"API CALL");
+        logger2.log(Status.PASS,"API TEST PASSED");
+        extent.flush();
+
+        //utilpackage
+        ExtentTest logger3 = extent.createTest("api package");
+        Utilmethods.getresponsetime(url);
+        logger3.log(Status.INFO,"API CALL");
+        logger3.log(Status.PASS,"API TEST PASSED");
+        extent.flush();
     }
+
+
 
 
 }
